a700c8e41a93dee8b84b70f98e2b20d22636906b
mddanish867 <mddanish867@gmail.com>
2024-09-01 12:06:47 +0530

    modified environment variable

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,6 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 export default async function Navbar() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  console.log(user)
   return (
     <nav className=" relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
       <div className="md:col-span-3">
